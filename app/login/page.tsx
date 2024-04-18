7ceb0d1a67f83516288a90c268dca1998873f892
Begli Kakabalov <begli.kaka@mail.ru>
2024-04-18 10:29:36 +0200

    Change for testing purpose

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,16 +13,13 @@ import styles from '../page.module.css'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import GoogleAccount from '../components/GoogleAccount'
-import { useSession } from 'next-auth/react'
 
 const LoginPage = () => {
-  const [user, setUser] = useState({
-    email: '',
-    password: ''
-  })
-
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const [error, setError] = useState(false)
-  const [wrongCredentials, setWrongCredentials] = useState('')
+  const [wrongPassword, setWrongPassword] = useState('')
+
   const router = useRouter()
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -30,34 +27,23 @@ const LoginPage = () => {
 
     try {
       const response = await signIn('credentials', {
-        email: user.email,
-        password: user.password,
+        email,
+        password,
         redirect: false
       })
-      console.log(response)
-
-      if (response?.ok) {
-        const form = event.target as HTMLFormElement
-        form.reset()
-        router.replace('/')
-      }
 
       setError(false)
       if (response?.error) {
-        setWrongCredentials('Invalid credentials')
+        setWrongPassword('Invalid credentials')
         return
       }
+      router.replace('/')
     } catch (error) {
       console.log(error)
     } finally {
       setError(true)
     }
   }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUser({ ...user, [e.target.name]: e.target.value })
-  }
-
   return (
     <div className={styles.login__container}>
       <Grid container justifyContent='center'>
@@ -67,9 +53,9 @@ const LoginPage = () => {
               Sign in
             </Typography>
             <GoogleAccount />
-            {wrongCredentials && (
+            {wrongPassword && (
               <Alert severity='error' sx={{ marginBottom: '10px' }}>
-                {wrongCredentials}
+                {wrongPassword}
               </Alert>
             )}
             <form onSubmit={handleSubmit}>
@@ -79,11 +65,10 @@ const LoginPage = () => {
                     label='Email'
                     variant='outlined'
                     size='small'
-                    name='email'
                     fullWidth
-                    onChange={handleChange}
-                    error={error && !user.email}
-                    helperText={error && !user.email && 'Enter the email'}
+                    onChange={e => setEmail(e.target.value)}
+                    error={error && !email}
+                    helperText={error && !email && 'Enter the email'}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -91,12 +76,11 @@ const LoginPage = () => {
                     label='Password'
                     type='password'
                     variant='outlined'
-                    name='password'
                     fullWidth
                     size='small'
-                    onChange={handleChange}
-                    error={error && !user.password}
-                    helperText={error && !user.password && 'Enter the password'}
+                    onChange={e => setPassword(e.target.value)}
+                    error={error && !password}
+                    helperText={error && !password && 'Enter the password'}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -131,3 +115,137 @@ const LoginPage = () => {
 }
 
 export default LoginPage
+
+// 'use client'
+// import React, { useState } from 'react'
+// import {
+//   Button,
+//   Grid,
+//   Link,
+//   Paper,
+//   TextField,
+//   Typography,
+//   Alert
+// } from '@mui/material'
+// import styles from '../page.module.css'
+// import { signIn } from 'next-auth/react'
+// import { useRouter } from 'next/navigation'
+// import GoogleAccount from '../components/GoogleAccount'
+// import { useSession } from 'next-auth/react'
+
+// const LoginPage = () => {
+//   const [user, setUser] = useState({
+//     email: '',
+//     password: ''
+//   })
+
+//   const [error, setError] = useState(false)
+//   const [wrongCredentials, setWrongCredentials] = useState('')
+//   const router = useRouter()
+
+//   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault()
+
+//     try {
+//       const response = await signIn('credentials', {
+//         email: user.email,
+//         password: user.password,
+//         redirect: false
+//       })
+//       console.log(response)
+
+//       if (response?.ok) {
+//         const form = event.target as HTMLFormElement
+//         form.reset()
+//         router.replace('/')
+//       }
+
+//       setError(false)
+//       if (response?.error) {
+//         setWrongCredentials('Invalid credentials')
+//         return
+//       }
+//     } catch (error) {
+//       console.log(error)
+//     } finally {
+//       setError(true)
+//     }
+//   }
+
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setUser({ ...user, [e.target.name]: e.target.value })
+//   }
+
+//   return (
+//     <div className={styles.login__container}>
+//       <Grid container justifyContent='center'>
+//         <Grid item xs={10} sm={8} md={6} lg={4}>
+//           <Paper elevation={3} style={{ padding: ' 30px' }}>
+//             <Typography variant='h5' gutterBottom>
+//               Sign in
+//             </Typography>
+//             <GoogleAccount />
+//             {wrongCredentials && (
+//               <Alert severity='error' sx={{ marginBottom: '10px' }}>
+//                 {wrongCredentials}
+//               </Alert>
+//             )}
+//             <form onSubmit={handleSubmit}>
+//               <Grid container spacing={2}>
+//                 <Grid item xs={12}>
+//                   <TextField
+//                     label='Email'
+//                     variant='outlined'
+//                     size='small'
+//                     name='email'
+//                     fullWidth
+//                     onChange={handleChange}
+//                     error={error && !user.email}
+//                     helperText={error && !user.email && 'Enter the email'}
+//                   />
+//                 </Grid>
+//                 <Grid item xs={12}>
+//                   <TextField
+//                     label='Password'
+//                     type='password'
+//                     variant='outlined'
+//                     name='password'
+//                     fullWidth
+//                     size='small'
+//                     onChange={handleChange}
+//                     error={error && !user.password}
+//                     helperText={error && !user.password && 'Enter the password'}
+//                   />
+//                 </Grid>
+//                 <Grid item xs={12}>
+//                   <Button
+//                     variant='contained'
+//                     color='primary'
+//                     type='submit'
+//                     fullWidth
+//                   >
+//                     Sign in
+//                   </Button>
+//                 </Grid>
+//                 <Grid item xs={12}>
+//                   <Typography variant='body2' gutterBottom>
+//                     Don't have an account?
+//                     <Link style={{ textDecoration: 'none' }} href={'/signup'}>
+//                       <span
+//                         style={{ marginLeft: '5px', textDecoration: 'none' }}
+//                       >
+//                         Sign up
+//                       </span>
+//                     </Link>
+//                   </Typography>
+//                 </Grid>
+//               </Grid>
+//             </form>
+//           </Paper>
+//         </Grid>
+//       </Grid>
+//     </div>
+//   )
+// }
+
+// export default LoginPage
